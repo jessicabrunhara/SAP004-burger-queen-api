30c8f8741351904c628706258ce78bd7c9b047c8
Jessica Brunhara <brunharaj@gmail.com>
2020-08-26 14:49:54 -0300

    banco de dados criado, tabelas Order e tabela Product também

---
+++ api/server/src/models/order.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Order = sequelize.define('Order', {
+    active: DataTypes.BOOLEAN,
+    table: DataTypes.INTEGER,
+    client_name: DataTypes.STRING,
+    waiter_name: DataTypes.STRING
+  }, {});
+  Order.associate = function(models) {
+    // associations can be defined here
+  };
+  return Order;
+};
