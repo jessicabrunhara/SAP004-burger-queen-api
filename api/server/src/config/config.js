1a26f2683ca1c701ed541b10c5b89d6b63f16ce6
Jessica Brunhara <brunharaj@gmail.com>
2020-08-28 20:21:36 -0300

    deploy

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   "production": {
     "username": process.env.DB_USER,
-    "password": process.env.DB_USER,
+    "password": process.env.DB_PASS,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "dialect": "postgres",
